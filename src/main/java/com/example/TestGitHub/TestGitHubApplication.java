96e61c517e7eaf58ff5812ab90e46b5afe639513
Dmitriy <soldatov.dmitriy123@gmail.com>
2025-09-19 19:30:13 +0700

    Initial commit

---
+++ src/main/java/com/example/TestGitHub/TestGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/TestGitHub/TestGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
